9bb09a9633ef3f099f0c94f17323b99c283ec92e
Hussein Rizk <husseinrizk117@gmail.com>
2025-03-29 19:20:59 +0200

    create all ui

--- lib/view/calculateview.dart
+++ lib/view/calculateview.dart
@@ -32,7 +32,7 @@ class _CalculateViewState extends State<CalculateView> {
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
-              fontSize: 24,
+              fontSize: 32,
             ),
           ),
           backgroundColor: Colors.black,
@@ -52,6 +52,70 @@ class _CalculateViewState extends State<CalculateView> {
                 ],
               ),
               SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  calculatebutton('Ac', Colors.black, Colors.grey),
+                  calculatebutton('+/-', Colors.black, Colors.grey),
+                  calculatebutton('%', Colors.black, Colors.grey),
+                  calculatebutton('/', Colors.white, Colors.orange),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  calculatebutton('7', Colors.black, Colors.grey),
+                  calculatebutton('8', Colors.black, Colors.grey),
+                  calculatebutton('9', Colors.black, Colors.grey),
+                  calculatebutton('X', Colors.white, Colors.orange),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  calculatebutton('4', Colors.black, Colors.grey),
+                  calculatebutton('5', Colors.black, Colors.grey),
+                  calculatebutton('6', Colors.black, Colors.grey),
+                  calculatebutton('-', Colors.white, Colors.orange),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  calculatebutton('1', Colors.black, Colors.grey),
+                  calculatebutton('2', Colors.black, Colors.grey),
+                  calculatebutton('3', Colors.black, Colors.grey),
+                  calculatebutton('+', Colors.white, Colors.orange),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      shape: StadiumBorder(),
+                      padding: EdgeInsets.only(
+                        left: 36,
+                        top: 12,
+                        bottom: 12,
+                        right: 92,
+                      ),
+                    ),
+                    child: Text(
+                      '0',
+                      style: TextStyle(color: Colors.black, fontSize: 36),
+                    ),
+                  ),
+                  calculatebutton('.', Colors.black, Colors.grey),
+                  calculatebutton('=', Colors.white, Colors.orange),
+                ],
+              ),
             ],
           ),
         ),
